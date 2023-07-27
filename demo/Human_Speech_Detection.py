9cc375f81f10e5df60221d97c50d52a5cd0918e7
Martin Sejas <mhsejas@gmail.com>
2023-07-27 21:46:34 +0200

    FInal Version

--- demo/Human_Speech_Detection.py
+++ demo/Human_Speech_Detection.py
@@ -4,7 +4,10 @@ import joblib
 import librosa
 import numpy as np
 import os
+import soundfile as sf
+import Visualization as V
 from pydub import AudioSegment
+from speechbrain.pretrained import SepformerSeparation as separator
 import tempfile
 import matplotlib.pyplot as plt
 from audiomentations import Compose, AddGaussianNoise, TimeStretch, PitchShift, Shift
@@ -15,15 +18,34 @@ MFCC_COUNT = 40
 TOP_DB = 20
 
 @st.cache_resource
-def get_model():
+def get_model_human_speech():
     return keras.models.load_model("HumanSpeechRecognition/model-human-speech-detection/model.h5")
 
 @st.cache_resource
 def get_encoder():
     return joblib.load("HumanSpeechRecognition/model-human-speech-detection/label_encoder.joblib")
 
-model = get_model()
-le = get_encoder()
+
+@st.cache_resource
+def get_model_noise():
+    return keras.models.load_model("NoiseDetection/model-nd/noise_model.h5")
+
+
+@st.cache_resource
+def get_encoder_noise():
+    return joblib.load("NoiseDetection/model-nd/noise_label_encoder.joblib")
+
+@st.cache_resource
+def load_model():
+    return separator.from_hparams(source="speechbrain/sepformer-whamr-enhancement", savedir='demo/pretrained_models/sepformer-whamr-enhancement')
+
+model = load_model()
+
+model_human_speech = get_model_human_speech()
+le_human_speech = get_encoder()
+
+model_noise = get_model_noise()
+le_noise = get_encoder_noise()
 
 def delete_file_after_delay(filename, delay=5):
 
@@ -90,7 +112,22 @@ def predict_audio_class(audio_file_path, model, le):
     prediction_label = le.inverse_transform(prediction)[0]
     return prediction_label
 
-st.title('Human Speech Detection')
+def extract_mfccs(file_path):
+    SAMPLE_RATE = 22050
+    MFCC_COUNT = 13
+    audio, _ = librosa.load(file_path, sr=SAMPLE_RATE, mono=True)
+    mfccs = librosa.feature.mfcc(y=audio, sr=SAMPLE_RATE, n_mfcc=MFCC_COUNT)
+    return np.mean(mfccs.T, axis=0)
+
+def predict_audio_class_noise(audio_file, model, le):
+    mfcc = extract_mfccs(audio_file)
+    mfcc = np.expand_dims(mfcc, axis=0) 
+    prediction_prob = model.predict(mfcc)
+    prediction = (prediction_prob > 0.5).astype("int32")
+    prediction_label = le.inverse_transform(prediction)[0]
+    return prediction_label
+
+st.title('Human Speech Detection and Enhancement')
 
 uploaded_file = st.file_uploader("Choose an audio file", type=['mp3', 'wav', 'flac'])
 
@@ -101,8 +138,48 @@ if uploaded_file is not None:
 
     st.audio(audio, format='audio/wav', sample_rate=SAMPLE_RATE)
 
-    prediction = predict_audio_class(tfile.name, model, le)
+    #Human speech detected
+    prediction = predict_audio_class(tfile.name, model_human_speech, le_human_speech)
+    
+    
 
-    st.write(f"The audio is predicted as: {prediction}")
+    #st.write(f"The audio is predicted as: {prediction}")
+    
+    
+    if(prediction == "human"):
+        st.write(f"Human voice detected! Detecting Quality \n\n")
+        with st.spinner("Detecting if audio is noisy..."):
+            noise_prediction = predict_audio_class_noise(tfile.name, model_noise, le_noise)
+            
+        st.write(f"The audio quality is: {noise_prediction}")
+        
+        if noise_prediction == "noise":
+            y_o, sr = librosa.load(tfile.name)
+            with st.spinner("Cleaning audio..."):
+                enhanced_audio = model.separate_file(path=f"{tfile.name}")
+            
+            st.divider()
+            
+            st.write("Cleaned Audio:")
+            
+            enhanced_data = enhanced_audio[:,:].detach().cpu().squeeze()
+            
+            
+            sf.write("data/clean.wav",enhanced_data,samplerate=8000)
+            st.audio("data/clean.wav")
+            
+            y_e, sr = librosa.load("data/clean.wav")
+            
+            st.subheader("Visualizations The Cleaned Audio vs Origianl Audio")
+            with st.expander("Click to see the comparison"):
+                st.write("Original Audio")
+                V.show_waveform(y_o/max(y_o),sr, color='blue')
+                st.write("Cleaned Audio")
+                # set the color differently
+                V.show_waveform(y_e,sr, color='red')
+                
+    else:
+        st.write(f"No Human Speech detected \n\n")
+    
 
-    delete_file_after_delay(tfile.name) 
+    #delete_file_after_delay(tfile.name) 
