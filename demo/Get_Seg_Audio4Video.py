8e3e791e802705825920598a9c2c01f6083c99cc
Martin Sejas <mhsejas@gmail.com>
2023-07-27 15:14:07 +0200

    Final working version

--- demo/Get_Seg_Audio4Video.py
+++ demo/Get_Seg_Audio4Video.py
@@ -1,39 +1,67 @@
+import streamlit as st
+import os
+import speech_recognition as speech_recon
+import json
 from pytube import YouTube
 from moviepy.editor import *
 from pydub.silence import split_on_silence
 from moviepy.editor import *
 from pydub import AudioSegment
-import os
+import concurrent.futures
+from tqdm import tqdm
 
+def create_directory_if_not_exists(directory_path):
+    if not os.path.exists(directory_path):
+        try:
+            os.makedirs(directory_path)
+            print(f"Directory '{directory_path}' created successfully.")
+        except OSError as e:
+            print(f"Error: Failed to create directory '{directory_path}': {e}")
+    else:
+        print(f"Directory '{directory_path}' already exists.")
 
-def download_youtube_video(youtube_url: str, output_path='youtube/'):
+# Example usage:
+VIDEO_OUTPUT_PATH = os.path.abspath(os.path.join('data', 'youtube'))
+create_directory_if_not_exists(VIDEO_OUTPUT_PATH)
+
+def download_youtube_video(youtube_url: str, output_path=VIDEO_OUTPUT_PATH):
     try:
         yt = YouTube(youtube_url)
         audio_mp4 = yt.streams.filter(only_audio=True)[1].download(output_path=output_path)
-        return yt._title, audio_mp4
+        return yt.title, audio_mp4
     except:
         pass
 
-
-def split_audio_from_video(video_path, output_path='youtube/'):
+def split_audio_from_video(video_path, output_path=VIDEO_OUTPUT_PATH):
     try:
         video = VideoFileClip(video_path)
         audio = video.audio
         audio_name = os.path.basename(video_path).split('.')[0]
-        audio_path = f'{output_path}/{audio_name}.wav'
+        audio_path = os.path.join(output_path, f'{audio_name}.wav')
         audio.write_audiofile(audio_path)
         return audio_path
     except Exception as e:
         print(e)
 
-
-def segment_audio(audio_path, audio_name, min_silence_len=700, silence_thresh=-50, output_path='youtube'):
-    audio = AudioSegment.from_file(audio_path)
+def segment_audio(audio_path, audio_name, min_silence_len=700, silence_thresh=-50, output_path=VIDEO_OUTPUT_PATH):
+    print(f"Trying to load audio from: {audio_path}")  # Add this print statement
+    audio = AudioSegment.from_file(audio_path,format="mp4")
     segments = split_on_silence(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh)
     
-    output_directory = f'{output_path}/{audio_name}_clips'
+    output_directory = os.path.join(output_path, f'{audio_name}_clips')
     output_paths = []
-    os.makedirs(output_directory, exist_ok=True)  # Create the directory if it doesn't exist
+    output_directory = output_directory.replace("\\","/")
+    display_path = output_directory.replace("?", "")
+    print("Display path:", display_path)   
+    output_directory = display_path
+    
+    try:
+        os.makedirs(output_directory, exist_ok=True)  # Create the directory if it doesn't exist
+        # Or do other operations with the original_path
+    except OSError as e:
+        print("Error:", e)
+    
+  
     for i, seg in enumerate(segments):
         output_file = os.path.join(output_directory, f'seg{i:04d}.wav')
         try:
@@ -44,9 +72,57 @@ def segment_audio(audio_path, audio_name, min_silence_len=700, silence_thresh=-5
     print(f'Audio segments are saved in {output_directory}')
     return segments, output_paths
 
-
-def concatenate_audio_segments(audios_path, audio_name, output_path='youtube'):
+def concatenate_audio_segments(audios_path, audio_name, output_path='data/youtube'):
     audios = sorted(audios_path, key=lambda x: int(os.path.basename(x).split('.')[0][-4:]))
     audio_segments = [AudioSegment.from_wav(audio) for audio in audios]
     combined_audio = sum(audio_segments)
-    combined_audio.export(f'output_path/{audio_name}_enhanced', format='wav')
+    combined_audio.export(os.path.join(output_path, f'{audio_name}_enhanced.wav'), format='wav')
+
+def download_youtube_audio(youtube_url):
+    try:
+        audio_name, audio_path = download_youtube_video(youtube_url)
+        print(f"Downloaded audio path: {audio_path}")  # Add this print statement
+        st.session_state.video_dict['audio_name'] = audio_name
+        st.session_state.video_dict['audio_path'] = audio_path
+        st.text(f'Youtube Video: {audio_name}')
+    except:
+        st.text('Please enter a valid youtube link')
+
+
+def sort_clip_paths_by_order(clip_paths):
+    return sorted(clip_paths, key=lambda x: int(os.path.basename(x).split('.')[0][-4:]))
+
+
+def transcribe_audio_segments():
+    def transcribe_single_audio(path):
+        r = speech_recon.Recognizer()
+        with speech_recon.AudioFile(path) as source:
+            audio_text = r.listen(source)
+            final_text = r.recognize_google(audio_data=audio_text)
+            return final_text
+
+    transcription = ''
+    clip_paths = st.session_state.video_dict['clip_paths']
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Start transcribing each audio segment concurrently
+        futures = [executor.submit(transcribe_single_audio, path) for path in clip_paths]
+
+        # Wait for all transcriptions to complete
+        concurrent.futures.wait(futures)
+
+        # Retrieve the results and combine them
+        for future in futures:
+            final_text = future.result()
+            transcription += ' ' + final_text + '\n'
+
+    st.session_state.video_dict['transcription'] = transcription
+    # st.text('Transcription has generated. You can read the transcription below or download it.')
+    # with st.expander("Transcription"):
+    #     st.write(transcription)
+
+def save_to_txt(data: dict, name: str, path: str = VIDEO_OUTPUT_PATH):
+    path = path.replace("\\","/")
+    name = name.replace("?", "")
+    with open(f"{path}/{name}.json", "w") as f:
+        json.dump(data, f)
---
+++ demo/pages/Upload_Audio.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import soundfile as sf
+from speechbrain.pretrained import SepformerSeparation as separator
+import librosa
+import speech_recognition as speech_recon
+import Visualization as V
+# import requests
+# import utility
+
+
+DATA_PATH="data/"
+
+@st.cache_resource
+def load_model():
+    return separator.from_hparams(source="speechbrain/sepformer-whamr-enhancement", savedir='demo/pretrained_models/sepformer-whamr-enhancement')
+
+model = load_model()
+
+st.title("  Clean and Transcribe your audio! ")
+
+
+audio_file = st.file_uploader(label="Upload audio",type=[".wav"])
+
+if audio_file is not None:
+    
+    # save the audio_file to DATA_PATH
+    with open(f"{DATA_PATH}{audio_file.name}","wb") as f:
+        f.write(audio_file.getbuffer())
+    
+    st.write("Noisy Audio:")
+    st.audio(audio_file)
+    
+    y_o, sr = librosa.load(audio_file)
+    # y_o,sr = librosa.load(f"{DATA_PATH}{audio_file.name}")
+    
+    WRITTEN_PATH = f"{DATA_PATH}sample.wav"
+
+    with st.spinner("Cleaning audio..."):
+        enhanced_audio = model.separate_file(path=f"{DATA_PATH}{audio_file.name}")
+        
+    st.divider()
+    
+    st.write("Cleaned Audio:")
+    
+    enhanced_data = enhanced_audio[:,:].detach().cpu().squeeze()
+    
+    
+    sf.write("data/clean.wav",enhanced_data,samplerate=8000)
+    #sf.write("data/clean.wav",enhanced_data_resampled,samplerate=16000)
+    st.audio("data/clean.wav")
+    
+    y_e, sr = librosa.load("data/clean.wav")
+    
+    st.subheader("Visualizations The Cleaned Audio vs Origianl Audio")
+    with st.expander("Click to see the comparison"):
+        st.write("Original Audio")
+        V.show_waveform(y_o/max(y_o),sr, color='blue')
+        st.write("Cleaned Audio")
+        # set the color differently
+        V.show_waveform(y_e,sr, color='red')
+    
+    
+    # API_URL_TRANS = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-fr"
+    # API_TOKEN = utility.API_TOKEN
+    # headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    # def query_translation(payload):
+    #     response = requests.post(API_URL_TRANS, headers=headers, json=payload)
+    #     return response.json()
+    
+    
+    st.subheader("Transcription")
+    r = speech_recon.Recognizer()
+
+    with speech_recon.AudioFile("data/clean.wav") as source:
+        audio_text = r.listen(source)
+        
+        with st.spinner("Transcribing..."):
+            final_text = r.recognize_google(audio_data=audio_text)
+        
+        st.write(final_text)
+        
+        # st.subheader("Translation of the transcription to French")
+        # output = query_translation({"inputs": final_text})
+        # translation = output[0]['translation_text']
+        # st.write(translation)
+        
+        # with open(f"translation/{audio_file.name}.txt", 'w') as f:
+        #     f.write(translation)
